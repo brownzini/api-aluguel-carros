d6b4b0733dc779c8954baa4460869240adb64e05
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-01 16:55:09 -0300

    alterar orm

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,14 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-       host
+      database:
+        process.env.NODE_ENV === "test"
+          ? "database_ignite"
+          : defaultOptions.database,
     })
   );
 };
