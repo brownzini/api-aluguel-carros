ba2f9fa30faadfa4d7b4b10eec193795e7d0e393
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-02 00:15:55 -0300

    make it better

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 3335
+EXPOSE 3000
 
 CMD ["npm","run", "dev"]
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,3 @@
 import { app } from "./app";
 
-app.listen(3335, () => console.log("Server is running on 3335!"));
+app.listen(3000, () => console.log("Server is running on 3000!"));
